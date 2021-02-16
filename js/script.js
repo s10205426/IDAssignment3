a3501fa0a750e0cf44069a8d26dc5a349d6b462e
Ho Zhao Yan <s10205426@connect.np.edu.sg>
2021-02-17 04:22:48 +0800

    Movii v1.0 (added Search Input + Movie Poster)

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const API_KEY = 'd8bf019d0cca372bd804735f172f67e8';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=d8bf019d0cca372bd804735f172f67e8';
+
+const searchId = document.querySelector('#search');
+const inputId = document.querySelector('#inputValue');
+const movieSearchable = document.querySelector('#movies-searchable');
+
+function movieSection(movies) { //Return movie image information
+    return movies.map((movie) => {
+        return `
+            <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
+        `;
+    })
+}
+
+function createMovieContainer(movies) {
+
+    //Create an element to have movie data appended on the spot
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const movieTemplate = //Contains movie data to be displayed
+    `
+        <section class="section">
+            ${movieSection(movies)} 
+        </section>
+        <div class="content">
+            <p id="content-close">X</p>
+        </div>
+    `;
+
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+}
+
+
+$(searchId).on("click", function(event) { //Initiate API search on click
+    event.preventDefault();
+    const value = inputId.value;
+    const newUrl = url + '&query=' + value;
+
+    fetch(newUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            const movies = data.results; //Contains movie data taken from API
+            const movieBlock = createMovieContainer(movies);
+            movieSearchable.appendChild(movieBlock);
+            console.log("Data: ", data);
+        })
+    console.log("Value: ", value);
+});
